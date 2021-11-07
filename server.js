0a198d7624ab5ebbbfef9e878dafa7b6e01d4773
juragi <juragi21@hotmail.com>
2021-11-07 13:33:41 +0900

    Add Database connection

--- server.js
+++ server.js
@@ -2,51 +2,45 @@ const express = require('express');
 const path = require('path');
 const app  = express();
 const port = process.env.PORT || 5000;
+const fs = require('fs');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+//const data = fs.readFileSync('./database.json');
+//const conf = JSON.parse(data);
+const conf = {
+  host: process.env.MARIA_HOST,
+  user: process.env.MARIA_USER,
+  password: process.env.MARIA_PASSWORD,
+  port: process.env.MARIA_PORT,
+  database: process.env.MARIA_PORT
+}
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+connection.connect();
 
 app.get('/api/hello', (req, res) => {
     res.send({message: "Hello Express"});
 });
 
-const customers = [
-    {
-      'id': 1,
-      'image':'https://placeimg.com/64/64/1',
-      'name': '홍길동',
-      'birthday': '961222',
-      'gender': '남자',
-      'job': '대학생'
-    },
-    {
-      'id':2,
-      'image':'https://placeimg.com/64/64/2',
-      'name': '홍길동2',
-      'birthday': '961224',
-      'gender': '남자',
-      'job': '대학생'
-    },
-    {
-      'id':3,
-      'image':'https://placeimg.com/64/64/3',
-      'name': '홍길동3',
-      'birthday': '961223',
-      'gender': '남자',
-      'job': 'none'
-    },
-    {
-        'id': 4,
-        'image': 'https://placeimg.com/64/64/4',
-        'name': '홍길동4',
-        'birthday': '961111',
-        'gender': '남자',
-        'job': 'none'
-    }
-  ];
+
 
 app.get("/api/customers", (req, res) => {
-    res.send(customers);
+    //res.send(customers);
+    connection.query(
+      "select * from customer",
+      (err, rows, fields) => {
+        res.send(rows);
+      }
+    )
 });
 
 app.use(express.static(path.join(__dirname, 'client/build')));
